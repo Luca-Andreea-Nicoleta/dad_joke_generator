5cb842d48898da1a1035a73141936162f2557636
Luca-Andreea <andreea.xen@gmail.com>
2023-07-18 11:02:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "gpSjVTLjyO5DVEiWToRgFw==jCnLEsSWuCwwC80H";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+  try {
+    jokeEl.innerText = "...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Wait please";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+
+    jokeEl.innerText = data[0].joke;
+  } catch (error) {
+    jokeEl.innerText = "An error happend, try again later";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+  }
+}
+
+btnEl.addEventListener("click", getJoke);
